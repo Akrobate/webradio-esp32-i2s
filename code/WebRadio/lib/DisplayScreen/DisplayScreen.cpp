d926e62ef648153e39244c1f1bc4f104e359dbd1
Akrobate <fedorov.artiom@gmail.com>
2024-09-23 22:07:09 +0200

    Added new screens

--- code/WebRadio/lib/DisplayScreen/DisplayScreen.cpp
+++ code/WebRadio/lib/DisplayScreen/DisplayScreen.cpp
@@ -3,8 +3,17 @@
 DisplayScreen::DisplayScreen() {
 }
 
+void DisplayScreen::injectBusinesState(BusinessState * business_state) {
+    this->business_state = business_state;
+}
+
+void DisplayScreen::injectStreamRepository(StreamRepository * stream_repository) {
+    this->stream_repository = stream_repository;
+}
+
 void DisplayScreen::init() {
     this->u8g2 = new U8G2_SH1106_128X64_NONAME_F_HW_I2C(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+    Serial.println("..................Init display");
     this->u8g2->begin();
     this->u8g2->enableUTF8Print();
 
@@ -12,19 +21,8 @@ void DisplayScreen::init() {
         [](void *arg){
             DisplayScreen * display_screen = (DisplayScreen *)arg;
             while(1) {
+                display_screen->displayStack();
                 vTaskDelay(pdMS_TO_TICKS(100));
-
-                if (display_screen->business_state->getIsConnectingToWifi()) {
-                    display_screen->connectingScreen();
-                    continue;
-                }
-
-                if (millis() - display_screen->business_state->getVolumeChangedAtMillis() < 4000) {
-                    display_screen->volumeScreen();
-                } else {
-                    display_screen->standbyScreen();
-                }
-   
             }
         },
         "displayTask",
@@ -35,29 +33,72 @@ void DisplayScreen::init() {
     );
 }
 
+
+void DisplayScreen::displayStack() {
+
+
+    if (
+        this->business_state->getInitingDevice() 
+        && !this->business_state->getIsConnectingToWifi()
+    ) {
+        this->splashScreen();
+        return;
+    }
+
+    if (this->business_state->getIsConnectingToWifi()) {
+        this->connectingScreen();
+        return;
+    }
+
+    if (!this->business_state->getIsConnectingToWifi() && !this->business_state->getDateTimeConfigured()) {
+        this->configuringTimeScreen();
+        return;
+    }
+
+
+    if (millis() - this->business_state->getVolumeChangedAtMillis() < 2000) {
+        this->volumeScreen();
+        return;
+    }
+
+    if (this->business_state->getPlayingStream() >= 0) {
+        this->playingStreamScreen();
+        return;
+    }
+
+    if (this->business_state->getPlayingVolume() == 1) {
+        this->streamSelectionScreen();
+        return;
+    }
+
+    this->standbyScreen();
+    
+}
+
+
 void DisplayScreen::clear() {
     this->u8g2->clearBuffer();
 }
 
 
-void DisplayScreen::demoScreen() {
- 
+void DisplayScreen::splashScreen() {
     this->clear();
-    String demo_string = "Démo"; 
-    this->u8g2->setFont(u8g2_font_spleen32x64_me); // u8g2_font_7x13B_tf
-    this->u8g2->setCursor(2, 64);
-    this->u8g2->print(demo_string);
+    String text = "WebRadio"; 
+    this->u8g2->setFont(u8g2_font_spleen16x32_me);
+
+    int text_width = this->u8g2->getUTF8Width(text.c_str());
+    int text_height = this->u8g2->getAscent() - this->u8g2->getDescent();
+
+    int x = this->u8g2->getDisplayWidth() / 2 - text_width / 2;
+    int y = this->u8g2->getDisplayHeight() / 2 + text_height / 2;
+
+    this->u8g2->drawUTF8(x, y, text.c_str());    
     this->display();
 }
 
 
 void DisplayScreen::display() {
     this->u8g2->sendBuffer();
-}
-
-
-void DisplayScreen::injectBusinesState(BusinessState * business_state) {
-    this->business_state = business_state;
 }
 
 
@@ -84,7 +125,6 @@ void DisplayScreen::infoScreen() {
 }
 
 
-
 void DisplayScreen::infoTestScreen() {
     this->clear();
 
@@ -101,7 +141,6 @@ void DisplayScreen::infoTestScreen() {
 }
 
 
-// Production screen
 void DisplayScreen::standbyScreen() {
     this->clear();
 
@@ -134,7 +173,135 @@ void DisplayScreen::standbyScreen() {
     x = this->u8g2->getDisplayWidth() - text_width;
     y = text_height;
     this->u8g2->drawUTF8(x, y, formated_temperature.c_str());  
+
+    this->display();
+}
+
+
+void DisplayScreen::configuringTimeScreen() {
+    this->clear();
+
+    int text_width = 0;
+    int text_height = 0;
+    int x = 0;
+    int y = 0;
+
+    String text = "setting time";
+    this->u8g2->setFont(u8g2_font_7x13_tf);
+    text_width = this->u8g2->getUTF8Width(text.c_str());
+    text_height = this->u8g2->getAscent() - this->u8g2->getDescent();
+    x = this->u8g2->getDisplayWidth() / 2 - text_width / 2;
+    y = this->u8g2->getDisplayHeight() / 2 + text_height / 2;
+    this->u8g2->drawUTF8(x, y, text.c_str());  
     
+    float temperature = this->business_state->getTemperature();
+    String formated_temperature = String(temperature) + " C";
+    this->u8g2->setFont(u8g2_font_6x12_tf);
+    text_width = this->u8g2->getUTF8Width(formated_temperature.c_str());
+    text_height = this->u8g2->getAscent() - this->u8g2->getDescent();
+    x = this->u8g2->getDisplayWidth() - text_width;
+    y = text_height;
+    this->u8g2->drawUTF8(x, y, formated_temperature.c_str());  
+    
+
+    this->u8g2->drawCircle(80, 48, 5, U8G2_DRAW_ALL);
+    this->u8g2->drawCircle(48, 48, 5, U8G2_DRAW_ALL);
+    this->u8g2->drawCircle(64, 48, 5, U8G2_DRAW_ALL);
+
+    int position = millis() / 1000 % 3;
+
+    switch (position) {
+        case 0:
+            this->u8g2->drawDisc(48, 48, 5, U8G2_DRAW_ALL);
+            break;
+        case 1:
+            this->u8g2->drawDisc(64, 48, 5, U8G2_DRAW_ALL);
+            break;
+        case 2:
+            this->u8g2->drawDisc(80, 48, 5, U8G2_DRAW_ALL);
+            break;    
+        default:
+            break;
+    }
+
+
+
+    this->display();
+}
+
+
+void DisplayScreen::playingStreamScreen() {
+    this->clear();
+
+    int text_width = 0;
+    int text_height = 0;
+    int x = 0;
+    int y = 0;
+
+    String formated_time = this->business_state->getFormatedTime();
+    this->u8g2->setFont(u8g2_font_6x12_tf);
+    text_height = this->u8g2->getAscent() - this->u8g2->getDescent();
+    x = 0;
+    y = text_height;
+    this->u8g2->drawUTF8(x, y, formated_time.c_str()); 
+
+    JsonObject stream = this->stream_repository->getStreamByIndex(this->business_state->getPlayingStream());
+    String playing_string = stream["name"];
+
+    // Autosizing
+    // u8g2_font_6x12_tf
+    // u8g2_font_7x13_tf
+    // u8g2_font_8x13_tf
+    // u8g2_font_9x15_tf
+    // u8g2_font_10x20_tf
+
+    int max_width = this->u8g2->getDisplayWidth() - 20;
+
+    this->u8g2->setFont(u8g2_font_10x20_tf);
+    text_width = this->u8g2->getUTF8Width(playing_string.c_str());
+
+    if (text_width > max_width) {
+        this->u8g2->setFont(u8g2_font_9x15_tf);
+        text_width = this->u8g2->getUTF8Width(playing_string.c_str()); 
+    }
+
+    if (text_width > max_width) {
+        this->u8g2->setFont(u8g2_font_8x13_tf);
+        text_width = this->u8g2->getUTF8Width(playing_string.c_str()); 
+    }
+
+    if (text_width > max_width) {
+        this->u8g2->setFont(u8g2_font_7x13_tf);
+        text_width = this->u8g2->getUTF8Width(playing_string.c_str()); 
+    }
+
+    if (text_width > max_width) {
+        this->u8g2->setFont(u8g2_font_6x12_tf);
+        text_width = this->u8g2->getUTF8Width(playing_string.c_str()); 
+    }
+
+    text_height = this->u8g2->getAscent() - this->u8g2->getDescent();
+    x = this->u8g2->getDisplayWidth() / 2 - text_width / 2;
+    y = this->u8g2->getDisplayHeight() / 2 + text_height / 2;
+    this->u8g2->drawUTF8(x, y, playing_string.c_str());  
+    
+
+    float temperature = this->business_state->getTemperature();
+    String formated_temperature = String(temperature) + " C";
+    this->u8g2->setFont(u8g2_font_6x12_tf);
+    text_width = this->u8g2->getUTF8Width(formated_temperature.c_str());
+    text_height = this->u8g2->getAscent() - this->u8g2->getDescent();
+    x = this->u8g2->getDisplayWidth() - text_width;
+    y = text_height;
+    this->u8g2->drawUTF8(x, y, formated_temperature.c_str());  
+
+    // Circle animation
+    int position = millis() / 200 % 6;
+    if (position > 3) {
+        position = 6 - position;
+    }
+    this->u8g2->drawCircle(64, 54, 1 + position, U8G2_DRAW_ALL);
+
     this->display();
 }
 
@@ -168,11 +335,12 @@ void DisplayScreen::volumeScreen() {
     this->display();
 }
 
+
 void DisplayScreen::connectingScreen() {
     this->clear();
 
     String text = "connecting";
-    this->u8g2->setFont(u8g2_font_7x13B_tf);
+    this->u8g2->setFont(u8g2_font_7x13_tf);
     int text_width = this->u8g2->getUTF8Width(text.c_str());
     int text_height = this->u8g2->getAscent() - this->u8g2->getDescent();
 
@@ -208,3 +376,25 @@ void DisplayScreen::connectingScreen() {
     this->display();
 }
 
+
+void DisplayScreen::streamSelectionScreen() {
+    this->clear();
+
+    // @todo: implement repository streams count
+    int streams_count = this->stream_repository->stream_count;
+    int x = 10;
+    this->u8g2->setFontMode(1);
+    this->u8g2->setFont(u8g2_font_6x12_tf);
+    this->u8g2->setDrawColor(1);
+    this->u8g2->drawBox(0, 26, 128, 12);
+
+    for (int i = 0; i < streams_count; i++) {
+        JsonObject stream = this->stream_repository->getStreamByIndex(i);
+        String stream_name = stream["name"];
+        int y = (i + 1) * 12;
+        this->u8g2->setDrawColor(2);
+        this->u8g2->drawUTF8(x, y, stream_name.c_str());
+    }
+
+    this->display();
+}
--- code/WebRadio/lib/DisplayScreen/DisplayScreen.h
+++ code/WebRadio/lib/DisplayScreen/DisplayScreen.h
@@ -5,30 +5,38 @@
 #include <U8g2lib.h>
 
 #include <BusinessState.h>
+#include <StreamRepository.h>
 
 class DisplayScreen {
 
     private:
         U8G2_SH1106_128X64_NONAME_F_HW_I2C * u8g2;
         BusinessState * business_state = nullptr;
+        StreamRepository * stream_repository = nullptr;
 
         int displayed_volume = 0;
 
     public:
         DisplayScreen();
         void injectBusinesState(BusinessState * business_state);
+        void injectStreamRepository(StreamRepository * stream_repository);
 
         void init();
         void clear();
         void display();
 
-        void demoScreen();
+        void displayStack();
+
         void infoScreen();
         void infoTestScreen();
 
+        void splashScreen();
         void standbyScreen();
+        void playingStreamScreen();
+        void configuringTimeScreen();
         void volumeScreen();
         void connectingScreen();
+        void streamSelectionScreen();
 
 };
 
