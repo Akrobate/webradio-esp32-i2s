93153950f8c85a9cd2da1957ef8dd39b450985dd
Akrobate <fedorov.artiom@gmail.com>
2024-10-01 18:31:07 +0200

    InputInterface prepared

--- code/WebRadio/lib/InputInterface/InputInterface.cpp
+++ code/WebRadio/lib/InputInterface/InputInterface.cpp
@@ -10,9 +10,103 @@ void InputInterface::injectBusinesState(BusinessState * business_state) {
 
 
 void InputInterface::init() {
+
+    pinMode(this->PIN_BTN_1, INPUT_PULLUP);
+    pinMode(this->PIN_BTN_2, INPUT_PULLUP);
+    pinMode(this->PIN_BTN_3, INPUT_PULLUP);
+    pinMode(this->PIN_BTN_4, INPUT_PULLUP);
+    pinMode(this->PIN_BTN_5, INPUT_PULLUP);
+    pinMode(this->PIN_BTN_6, INPUT_PULLUP);
+    
+    this->button_a_up = new Toggle(this->PIN_BTN_1);
+    this->button_a_down = new Toggle(this->PIN_BTN_2);
+    this->button_a_validate = new Toggle(this->PIN_BTN_3);
+    this->button_b_up = new Toggle(this->PIN_BTN_4);
+    this->button_b_down = new Toggle(this->PIN_BTN_5);
+    this->button_b_validate = new Toggle(this->PIN_BTN_6);
 }
 
 
 void InputInterface::update() {
+
+    this->button_a_up->poll();
+    this->button_a_down->poll();
+    this->button_a_validate->poll();
+    this->button_b_up->poll();
+    this->button_b_down->poll();
+    this->button_b_validate->poll();
+
+
+    /*
+    if (this->button_a_up->onPress()) {
+        Serial.println("Button A UP pressed");
+    }
+    if (this->button_a_down->onPress()) {
+        Serial.println("Button A DOWN pressed");
+    }
+    if (this->button_a_validate->onPress()) {
+        Serial.println("Button A VALIDATE pressed");
+    }
+
+    if (this->button_b_up->onPress()) {
+        Serial.println("Button B UP pressed");
+    }
+    if (this->button_b_down->onPress()) {
+        Serial.println("Button B DOWN pressed");
+    }
+    if (this->button_b_validate->onPress()) {
+        Serial.println("Button B VALIDATE pressed");
+    }
+    */
+
+   this->processBusinessRules();
 }
+
+bool InputInterface::onVolumeUp() {
+    return this->button_a_up->onPress();
+}
+
+bool InputInterface::onVolumeDown() {
+    return this->button_a_down->onPress();
+}
+
+bool InputInterface::onStreamNext() {
+    return this->button_b_down->onPress();
+}
+
+bool InputInterface::onStreamPrevious() {
+    return this->button_b_up->onPress();
+}
+
+
+void InputInterface::processBusinessRules() {
+
+    if (this->onVolumeUp()) {
+        this->business_state->lock();
+        this->business_state->setPlayingVolume(this->business_state->getPlayingVolume() + 1);
+        this->business_state->unlock();
+    }
+
+    if (this->onVolumeDown()) {
+        this->business_state->lock();
+        this->business_state->setPlayingVolume(this->business_state->getPlayingVolume() - 1);
+        this->business_state->unlock();
+    }
+
+    if (this->onStreamPrevious()) {
+        this->business_state->lock();
+        this->business_state->setPlayingStream(this->business_state->getPlayingStream() - 1);
+        this->business_state->unlock();
+    }
+
+    if (this->onStreamNext()) {
+        this->business_state->lock();
+        this->business_state->setPlayingStream(this->business_state->getPlayingStream() + 1);
+        this->business_state->unlock();
+    }
+}
+
+
+
+
 
--- code/WebRadio/lib/InputInterface/InputInterface.h
+++ code/WebRadio/lib/InputInterface/InputInterface.h
@@ -1,21 +1,49 @@
 #ifndef InputInterface_h
 #define InputInterface_h
 
-#include <BusinessState.h>
 #include <Arduino.h>
+#include <Toggle.h>
+
+#include <BusinessState.h>
 
 class InputInterface {
 
     private:
         BusinessState * business_state = nullptr;
 
+        // pins to aviod: 0, 2, 12, 15, 6-11
+        unsigned int PIN_BTN_1 = 16; // L-UP
+        unsigned int PIN_BTN_2 = 23; // L-DOWN
+        unsigned int PIN_BTN_3 = 17; // L-Validate
+
+        unsigned int PIN_BTN_4 = 5; // R-UP
+        unsigned int PIN_BTN_5 = 19; // R-DWON
+        unsigned int PIN_BTN_6 = 18; // R-Validate
+
+        bool onVolumeUp();
+        bool onVolumeDown();
+        bool onStreamNext();
+        bool onStreamPrevious();
+
     public:
+
+        Toggle * button_a_up = nullptr;
+        Toggle * button_a_down = nullptr;
+        Toggle * button_a_validate = nullptr;
+
+        Toggle * button_b_up = nullptr;
+        Toggle * button_b_down = nullptr;
+        Toggle * button_b_validate = nullptr;
+
 
         InputInterface();
         void injectBusinesState(BusinessState * business_state);
         
+
         void init();
         void update();
+
+        void processBusinessRules();
 
 };
 
