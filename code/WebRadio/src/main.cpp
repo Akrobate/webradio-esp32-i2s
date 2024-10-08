6228145cc54baf38ce01281e9854c661262037fb
Akrobate <fedorov.artiom@gmail.com>
2024-10-08 18:22:14 +0200

    Organized injections

--- code/WebRadio/src/main.cpp
+++ code/WebRadio/src/main.cpp
@@ -36,43 +36,46 @@ int loops = 0;
 void setup() {
     //WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
     business_state->setInitingDevice(true);
-    Serial.begin(115200);
 
+    // @todo: inject all dependencies should be done in a single place
     display_screen->injectBusinesState(business_state);
     display_screen->injectStreamRepository(stream_repository);
-    display_screen->init();
-
     device_system->injectBusinesState(business_state);
-    device_system->configureTimeTask();
-    device_system->init();
-
-    // Repositorories loads
     configuration_repository->injectBusinessState(business_state);
-    configuration_repository->load();
-    network_credential_repository->load();
-    stream_repository->load();
-
     wifi_networking->injectNetworkCredentialRepository(network_credential_repository);
     wifi_networking->injectBusinessState(business_state);
-    wifi_networking->startAP();
-    wifi_networking->scan();
-    wifi_networking->init();
-    
     server->injectWifiNetworking(wifi_networking);
     server->injectNetworkCredentialRepository(network_credential_repository);
     server->injectStreamRepository(stream_repository);
     server->injectBusinessState(business_state);
-    server->init();
-    server->begin();
-    
     bmp_180_probe->injectBusinesState(business_state);
-    bmp_180_probe->init();
-
     audio_process->injectBusinesState(business_state);
     audio_process->injectStreamRepository(stream_repository);
+    input_interface->injectBusinesState(business_state);
+
+    Serial.begin(115200);
+
+    display_screen->init();
+
+    device_system->configureTimeTask();
+    device_system->init();
+
+    // Repositorories loads
+    configuration_repository->load();
+    network_credential_repository->load();
+    stream_repository->load();
+
+    wifi_networking->startAP();
+    wifi_networking->scan();
+    wifi_networking->init();
+    
+    server->init();
+    server->begin();
+
+    bmp_180_probe->init();
+
     audio_process->init();
 
-    input_interface->injectBusinesState(business_state);
     input_interface->init();
 
     business_state->setInitingDevice(false);
