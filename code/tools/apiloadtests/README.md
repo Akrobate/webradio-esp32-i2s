fecf26afee6bf747d7c1c6ca3012b30057fdc9c8
Akrobate <fedorov.artiom@gmail.com>
2025-03-07 18:05:25 +0100

    Added documentation to loadtests

--- code/tools/apiloadtests/README.md
+++ code/tools/apiloadtests/README.md
@@ -1,1 +1,15 @@
-@todo
+# ApiLoadTest (tool)
+
+## Launch tests
+
+```sh
+node main.js
+```
+
+## Configure tests
+
+```js
+// Configurations:
+const API_URL = 'http://192.168.1.25';
+const REQUEST_COUNT = 10000;
+```
--- code/tools/apiloadtests/main.js
+++ code/tools/apiloadtests/main.js
@@ -1,55 +1,43 @@
+'use strict'
+
 const axios = require('axios');
 
-
-// URL cible pour le test de charge
-const url_list = [
-    'http://192.168.1.25/',
-    'http://192.168.1.25/index.js.gz',
-    'http://192.168.1.25/repository.js.gz',
-    'http://192.168.1.25/style.css.gz',
-];
-
-// Nombre de requêtes à envoyer pour le test de charge
+// Configurations:
+const API_URL = 'http://192.168.1.25';
 const REQUEST_COUNT = 10000;
 
-// Fonction pour envoyer une seule requête
-async function sendRequest(url) {
-  try {
-    const startTime = Date.now();  // Enregistrer le temps de départ
-    const response = await axios.get(url);
-    const endTime = Date.now();    // Enregistrer le temps de fin
 
-    // Afficher le statut et le temps pris pour chaque requête
-    console.log(`Request ${url} - Status: ${response.status}, Time: ${endTime - startTime} ms`);
-  } catch (error) {
-    // Gérer les erreurs lors de la requête
-    console.log(`Request ${url} failed: ${error.message}`);
-  }
+const url_list = [
+    `${API_URL}`,
+    `${API_URL}/index.js.gz`,
+    `${API_URL}/repository.js.gz`,
+    `${API_URL}/style.css.gz`,
+];
+
+async function sendRequest(url) {
+    try {
+        const startTime = Date.now();
+        const response = await axios.get(url);
+        const endTime = Date.now();
+        console.log(`Request ${url} - Status: ${response.status}, Time: ${endTime - startTime} ms`);
+    } catch (error) {
+        console.log(`Request ${url} failed: ${error.message}`);
+    }
 }
 
-// Fonction pour exécuter le test de charge
 async function runLoadTest() {
-
-
-  // Attendre que toutes les requêtes soient terminées
-  for (let i = 0; i < REQUEST_COUNT; i++) {
+    for (let i = 0; i < REQUEST_COUNT; i++) {
         const promises = [];
-
-        // Envoyer plusieurs requêtes simultanées
         for (let url of url_list) {
-        promises.push(sendRequest(url));
+            promises.push(sendRequest(url));
         }
-
         const startTime = Date.now();
         await Promise.all(promises);
-        const endTime = Date.now();    // Enregistrer le temps de fin
-        
+        const endTime = Date.now();
         console.log(`==========> Request ${i} - Time: ${endTime - startTime} ms`);
-
-  }
-  console.log(`Finished sending ${REQUEST_COUNT} requests.`);
+    }
+    console.log(`Finished sending ${REQUEST_COUNT} requests.`);
 }
 
-// Lancer le test de charge
 runLoadTest();
 
